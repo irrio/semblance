55e4a4c967ccf7616c9315a7e470e2bb4d43f16c
Taylor <taylor@irreducible.io>
2024-08-10 11:55:04 -0600

    Clean up main.c

--- src/main.c
+++ src/main.c
@@ -1,35 +1,43 @@
 
+#include <complex.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include "cli.h"
 #include "wmod.h"
 #include "wbin.h"
 
-int main(int argc, char *argv[]) {
-    CliArgs args;
-    int cli_err = cli_parse(&args, argc, argv);
-
+void cli_parse_or_exit(CliArgs *args, int argc, char *argv[]) {
+    int cli_err = cli_parse(args, argc, argv);
     if (cli_err != 0) {
         printf("Failed to parse arguments: %s\n", cli_str_error(cli_err));
         printf("Usage:\n\t%s\n", cli_usage_str());
-        return 1;
+        exit(1);
     }
+}
 
-    WasmModule wmod;
-    WasmDecodeResult result = wbin_read_module(args.path, &wmod);
-
+void wbin_read_module_or_exit(CliArgs *args, WasmModule *wmod) {
+    WasmDecodeResult result = wbin_read_module(args->path, wmod);
     if (!wbin_is_ok(result)) {
         printf(
             "Failed to load wasm module at \"%s\": %s",
-            args.path,
+            args->path,
             wbin_explain_error_code(result)
         );
         if (wbin_error_has_cause(result)) {
             printf(" (%s)", wbin_explain_error_cause(result));
         }
         printf("\n");
-        return 2;
+        exit(2);
     }
+}
+
+int main(int argc, char *argv[]) {
+    CliArgs args;
+    WasmModule wmod;
+
+    cli_parse_or_exit(&args, argc, argv);
+    wbin_read_module_or_exit(&args, &wmod);
 
     return 0;
 }
