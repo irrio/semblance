44f3ca54514ba9b3da1a06391d32a35fe127bafc
Taylor <taylor@irreducible.io>
2024-11-15 13:12:17 -0700

    Clean up module instance allocation

--- src/wrun.c
+++ src/wrun.c
@@ -69,11 +69,33 @@ wasm_func_addr_t wrun_store_alloc_func(WasmStore *store, WasmModuleInst *winst, 
     return vec_push_back(&store->funcs, sizeof(WasmFuncInst), &finst) + 1;
 }
 
+wasm_func_addr_t *wrun_store_alloc_funcs(WasmStore *store, WasmModuleInst *winst, VEC(WasmFunc) *funcs) {
+    VEC(wasm_func_addr_t) funcaddrs;
+    vec_init(&funcaddrs);
+    for (size_t i = 0; i < funcs->len; i++) {
+        WasmFunc *func = funcs->ptr + (i * sizeof(WasmFunc));
+        wasm_func_addr_t funcaddr = wrun_store_alloc_func(store, winst, func);
+        vec_push_back(&funcaddrs, sizeof(wasm_func_addr_t), &funcaddr);
+    }
+    return funcaddrs.ptr;
+}
+
 wasm_table_addr_t wrun_store_alloc_table(WasmStore *store, WasmTable *table, WasmRefValue initval) {
     WasmTableInst tinst;
     tinst.tabletype = *table;
     vec_init_with_size(&tinst.elems, sizeof(WasmRefValue), table->limits.min, &initval);
     return vec_push_back(&store->tables, sizeof(WasmTableInst), &tinst) + 1;
+}
+
+wasm_table_addr_t *wrun_store_alloc_tables(WasmStore *store, VEC(WasmTable) *tables) {
+    VEC(wasm_table_addr_t) tableaddrs;
+    vec_init(&tableaddrs);
+    for (size_t i = 0; i < tables->len; i++) {
+        WasmTable *table = tables->ptr + (i * sizeof(WasmTable));
+        wasm_table_addr_t tableaddr = wrun_store_alloc_table(store, table, 0);
+        vec_push_back(&tableaddrs, sizeof(wasm_table_addr_t), &tableaddr);
+    }
+    return tableaddrs.ptr;
 }
 
 wasm_mem_addr_t wrun_store_alloc_mem(WasmStore *store, WasmMemType *mem) {
@@ -83,11 +105,34 @@ wasm_mem_addr_t wrun_store_alloc_mem(WasmStore *store, WasmMemType *mem) {
     return vec_push_back(&store->mems, sizeof(WasmMemInst), &minst) + 1;
 }
 
+wasm_mem_addr_t *wrun_store_alloc_mems(WasmStore *store, VEC(WasmMemType) *mems) {
+    VEC(wasm_table_addr_t) memaddrs;
+    vec_init(&memaddrs);
+    for (size_t i = 0; i < mems->len; i++) {
+        WasmMemType *mem = mems->ptr + (i * sizeof(WasmMemType));
+        wasm_mem_addr_t memaddr = wrun_store_alloc_mem(store, mem);
+        vec_push_back(&memaddrs, sizeof(wasm_mem_addr_t), &memaddr);
+    }
+    return memaddrs.ptr;
+}
+
 wasm_global_addr_t wrun_store_alloc_global(WasmStore *store, WasmGlobalType *globaltype, WasmValue val) {
     WasmGlobalInst ginst;
     ginst.globaltype = *globaltype;
     ginst.val = val;
     return vec_push_back(&store->globals, sizeof(WasmGlobalInst), &ginst) + 1;
+}
+
+wasm_global_addr_t *wrun_store_alloc_globals(WasmStore *store, VEC(WasmGlobalType) *globals, VEC(WasmValue) *globalinit) {
+    VEC(wasm_global_addr_t) globaladdrs;
+    vec_init(&globaladdrs);
+    for (size_t i = 0; i < globals->len; i++) {
+        WasmGlobal *global = globals->ptr + (i * sizeof(WasmGlobal));
+        WasmValue initval = *(WasmValue *)vec_at(globalinit, sizeof(WasmValue), i);
+        wasm_global_addr_t globaladdr = wrun_store_alloc_global(store, &global->globaltype, initval);
+        vec_push_back(&globaladdrs, sizeof(wasm_global_addr_t), &globaladdr);
+    }
+    return globaladdrs.ptr;
 }
 
 wasm_elem_addr_t wrun_store_alloc_elem(WasmStore *store, WasmElem *elem, VEC(WasmRefValue) *references) {
@@ -98,10 +143,33 @@ wasm_elem_addr_t wrun_store_alloc_elem(WasmStore *store, WasmElem *elem, VEC(Was
     return vec_push_back(&store->elems, sizeof(WasmElemInst), &einst) + 1;
 }
 
+wasm_elem_addr_t *wrun_store_alloc_elems(WasmStore *store, VEC(WasmElem) *elems, VEC(VEC(WasmRefValue)) *references) {
+    VEC(wasm_elem_addr_t) elemaddrs;
+    vec_init(&elemaddrs);
+    for (size_t i = 0; i < elems->len; i++) {
+        WasmElem *elem = elems->ptr + (i * sizeof(WasmElem));
+        Vec *references = vec_at(references, sizeof(Vec), i);
+        wasm_elem_addr_t elemaddr = wrun_store_alloc_elem(store, elem, references);
+        vec_push_back(&elemaddrs, sizeof(wasm_elem_addr_t), &elemaddr);
+    }
+    return elemaddrs.ptr;
+}
+
 wasm_data_addr_t wrun_store_alloc_data(WasmStore *store, WasmData *wdata) {
     WasmDataInst dinst;
     dinst.bytes= wdata->bytes;
     return vec_push_back(&store->datas, sizeof(WasmDataInst), &dinst) + 1;
+}
+
+wasm_data_addr_t *wrun_store_alloc_datas(WasmStore *store, VEC(WasmData) *wdatas) {
+    VEC(wasm_data_addr_t) dataaddrs;
+    vec_init(&dataaddrs);
+    for (size_t i = 0; i < wdatas->len; i++) {
+        WasmData *wdata = wdatas->ptr + (i * sizeof(WasmData));
+        wasm_data_addr_t dataaddr = wrun_store_alloc_data(store, wdata);
+        vec_push_back(&dataaddrs, sizeof(wasm_data_addr_t), &dataaddr);
+    }
+    return dataaddrs.ptr;
 }
 
 void wrun_init_params_init(WasmInitParams *params) {
@@ -142,76 +210,14 @@ void wrun_free_decomposed_imports(WasmDecomposedImports *decomposed) {
     vec_free(&decomposed->globals);
 }
 
-WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, WasmInitParams *params) {
-    assert(params->globalinit.len == wmod->globals.len);
-    assert(params->imports.len == wmod->imports.len);
-    assert(params->references.len == wmod->elems.len);
-
-    WasmModuleInst *winst = malloc(sizeof(WasmModuleInst));
-    winst->types = wmod->types.ptr;
-
-    VEC(wasm_func_addr_t) funcaddrs;
-    vec_init(&funcaddrs);
-    for (size_t i = 0; i < wmod->funcs.len; i++) {
-        WasmFunc *func = wmod->funcs.ptr + (i * sizeof(WasmFunc));
-        wasm_func_addr_t funcaddr = wrun_store_alloc_func(store, winst, func);
-        vec_push_back(&funcaddrs, sizeof(wasm_func_addr_t), &funcaddr);
-    }
-    winst->funcaddrs = funcaddrs.ptr;
-
-    VEC(wasm_table_addr_t) tableaddrs;
-    vec_init(&tableaddrs);
-    for (size_t i = 0; i < wmod->tables.len; i++) {
-        WasmTable *table = wmod->tables.ptr + (i * sizeof(WasmTable));
-        wasm_table_addr_t tableaddr = wrun_store_alloc_table(store, table, 0);
-        vec_push_back(&tableaddrs, sizeof(wasm_table_addr_t), &tableaddr);
-    }
-    winst->tableaddrs = tableaddrs.ptr;
-
-    VEC(wasm_table_addr_t) memaddrs;
-    vec_init(&memaddrs);
-    for (size_t i = 0; i < wmod->mems.len; i++) {
-        WasmMemType *mem = wmod->mems.ptr + (i * sizeof(WasmMemType));
-        wasm_mem_addr_t memaddr = wrun_store_alloc_mem(store, mem);
-        vec_push_back(&memaddrs, sizeof(wasm_mem_addr_t), &memaddr);
-    }
-    winst->memaddrs = memaddrs.ptr;
-
-    VEC(wasm_global_addr_t) globaladdrs;
-    vec_init(&globaladdrs);
-    for (size_t i = 0; i < wmod->globals.len; i++) {
-        WasmGlobal *global = wmod->globals.ptr + (i * sizeof(WasmGlobal));
-        WasmValue initval = *(WasmValue *)vec_at(&params->globalinit, sizeof(WasmValue), i);
-        wasm_global_addr_t globaladdr = wrun_store_alloc_global(store, &global->globaltype, initval);
-        vec_push_back(&globaladdrs, sizeof(wasm_global_addr_t), &globaladdr);
-    }
-    winst->globaladdrs = globaladdrs.ptr;
-
-    VEC(wasm_elem_addr_t) elemaddrs;
-    vec_init(&elemaddrs);
-    for (size_t i = 0; i < wmod->elems.len; i++) {
-        WasmElem *elem = wmod->elems.ptr + (i * sizeof(WasmElem));
-        Vec *references = vec_at(&params->references, sizeof(Vec), i);
-        wasm_elem_addr_t elemaddr = wrun_store_alloc_elem(store, elem, references);
-        vec_push_back(&elemaddrs, sizeof(wasm_elem_addr_t), &elemaddr);
-    }
-    winst->elemaddrs = elemaddrs.ptr;
-
-    VEC(wasm_data_addr_t) dataaddrs;
-    vec_init(&dataaddrs);
-    for (size_t i = 0; i < wmod->datas.len; i++) {
-        WasmData *wdata = wmod->datas.ptr + (i * sizeof(WasmData));
-        wasm_data_addr_t dataaddr = wrun_store_alloc_data(store, wdata);
-        vec_push_back(&dataaddrs, sizeof(wasm_data_addr_t), &dataaddr);
-    }
-    winst->dataaddrs = dataaddrs.ptr;
-
+WasmExportInst *wrun_instance_assign_exports(VEC(WasmExport) *exports, VEC(WasmExternVal) *imports, WasmModuleInst *winst) {
     WasmDecomposedImports decomposed;
-    wrun_decompose_imports(&params->imports, &decomposed);
-    VEC(WasmExportInst) exports;
-    vec_init(&exports);
-    for (size_t i = 0; i < wmod->exports.len; i++) {
-        WasmExport *wexp = wmod->exports.ptr + (i * sizeof(WasmExport));
+    wrun_decompose_imports(imports, &decomposed);
+
+    VEC(WasmExportInst) out;
+    vec_init(&out);
+    for (size_t i = 0; i < exports->len; i++) {
+        WasmExport *wexp = exports->ptr + (i * sizeof(WasmExport));
         WasmExportInst inst;
         inst.name = wexp->name;
         switch (wexp->desc.kind) {
@@ -221,7 +227,7 @@ WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, Wasm
                 if (idx < decomposed.mems.len) {
                     inst.val.val.mem = *(wasm_mem_addr_t*)vec_at(&decomposed.mems, sizeof(wasm_mem_addr_t), idx);
                 } else {
-                    inst.val.val.mem = *(wasm_mem_addr_t*)vec_at(&memaddrs, sizeof(wasm_mem_addr_t), idx - decomposed.mems.len);
+                    inst.val.val.mem = winst->memaddrs[idx - decomposed.mems.len];
                 }
                 break;
             }
@@ -231,7 +237,7 @@ WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, Wasm
                 if (idx < decomposed.funcs.len) {
                     inst.val.val.func = *(wasm_func_addr_t*)vec_at(&decomposed.funcs, sizeof(wasm_func_addr_t), idx);
                 } else {
-                    inst.val.val.func = *(wasm_func_addr_t*)vec_at(&funcaddrs, sizeof(wasm_func_addr_t), idx - decomposed.funcs.len);
+                    inst.val.val.func = winst->funcaddrs[idx - decomposed.funcs.len];
                 }
                 break;
             }
@@ -241,7 +247,7 @@ WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, Wasm
                 if (idx < decomposed.tables.len) {
                     inst.val.val.table = *(wasm_table_addr_t*)vec_at(&decomposed.tables, sizeof(wasm_table_addr_t), idx);
                 } else {
-                    inst.val.val.table = *(wasm_table_addr_t*)vec_at(&tableaddrs, sizeof(wasm_table_addr_t), idx - decomposed.tables.len);
+                    inst.val.val.table = winst->tableaddrs[idx - decomposed.tables.len];
                 }
                 break;
             }
@@ -251,15 +257,34 @@ WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, Wasm
                 if (idx < decomposed.globals.len) {
                     inst.val.val.global = *(wasm_global_addr_t*)vec_at(&decomposed.globals, sizeof(wasm_global_addr_t), idx);
                 } else {
-                    inst.val.val.global = *(wasm_global_addr_t*)vec_at(&globaladdrs, sizeof(wasm_global_addr_t), idx - decomposed.globals.len);
+                    inst.val.val.global = winst->globaladdrs[idx - decomposed.globals.len];
                 }
                 break;
             }
         }
-        vec_push_back(&exports, sizeof(WasmExportInst), &inst);
+        vec_push_back(&out, sizeof(WasmExportInst), &inst);
     }
+
     wrun_free_decomposed_imports(&decomposed);
-    winst->exports = exports.ptr;
+    return out.ptr;
+}
+
+WasmModuleInst *wrun_store_alloc_module(WasmStore *store, WasmModule *wmod, WasmInitParams *params) {
+    assert(params->globalinit.len == wmod->globals.len);
+    assert(params->imports.len == wmod->imports.len);
+    assert(params->references.len == wmod->elems.len);
+
+    WasmModuleInst *winst = malloc(sizeof(WasmModuleInst));
+
+    winst->types = wmod->types.ptr;
+    winst->funcaddrs = wrun_store_alloc_funcs(store, winst, &wmod->funcs);
+    winst->tableaddrs = wrun_store_alloc_tables(store, &wmod->tables);
+    winst->memaddrs = wrun_store_alloc_mems(store, &wmod->mems);
+    winst->globaladdrs = wrun_store_alloc_globals(store, &wmod->globals, &params->globalinit);
+    winst->elemaddrs = wrun_store_alloc_elems(store, &wmod->elems, &params->references);
+    winst->dataaddrs = wrun_store_alloc_datas(store, &wmod->datas);
+    winst->exports = wrun_instance_assign_exports(&wmod->exports, &params->imports, winst);
+
     return winst;
 }
 
