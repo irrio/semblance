246bcec56098dc6b10d77943e6489c5341175b54
Taylor <taylor@irreducible.io>
2026-04-28 00:14:10 -0600

    [crates/semblance] Refactor WasmModule::read to allocate exactly enough memory to load module into memory

--- crates/semblance/src/module.rs
+++ crates/semblance/src/module.rs
@@ -14,10 +14,13 @@ pub use valid::{WasmValidationError, WasmValidationResult, validate};
 
 impl repr::WasmModule {
     pub fn read(path: &Path) -> Result<Self, WasmReadError> {
-        let mut f = File::open(path)?;
-        let mut buf = Vec::new();
-        f.read_to_end(&mut buf)?;
-        let bytes = buf.into_boxed_slice();
+        let bytes = {
+            let mut f = File::open(path)?;
+            let meta = f.metadata()?;
+            let mut buf = Vec::with_capacity(meta.len() as usize);
+            f.read_to_end(&mut buf)?;
+            buf.into_boxed_slice()
+        };
         let wmod = bin::decode(&bytes)?;
         let valid = validate(wmod)?;
         Ok(valid)
